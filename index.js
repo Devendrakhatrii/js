1f9e512b65b3d9b6a14e5c4481957f371290cd0e
Devendra <devendrakhatri904@gmail.com>
2023-12-22 17:17:21 +0545

    arrays

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log("hello world!");
+
+
+
+const prices=[250,645,300,900,50];
+
+
+
+for(let price of prices){
+
+    // console.log(price);
+    let dis=price*(10/100);
+    // console.log(dis);
+
+    price-=dis;
+    console.log(price);
+
+}
+
+console.log(` After discounted price = ${prices}`);
